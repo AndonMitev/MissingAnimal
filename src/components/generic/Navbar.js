495c7c269de750f80e4835f12d9d08168eca1665
Andon Mitev <mitevandon94@gmail.com>
2019-09-29 02:55:26 -0700

    Create navbar and set login and register page

---
+++ src/components/generic/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="#">
+          Login
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          Register
+        </a>
+      </li>
+    </ul>
+  </nav>
+);

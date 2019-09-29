c6d1084d1210a310feb752847a4c5fe8e6141d5e
Andon Mitev <mitevandon94@gmail.com>
2019-09-29 05:54:09 -0700

    Init store

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Navbar from './components/common/Navbar';
 import { Switch, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
